8d1b5301b4c124e58b1ade07118b98b8ac9a9ae9
Colin Sauze <cos@aber.ac.uk>
2018-04-20 16:54:06 +0100

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#get park boundary data
+./download_boundaries.sh
+
+git submodule update --init --recursive
+cd darknet
+make
+wget https://pjreddie.com/media/files/yolov3.weights
+cd ..
+
+#annoying hack to make sed replace the path
+libdarknet=`pwd`"/darknet/libdarknet.so"
+echo $libdarknet | sed 's/\//\\\//g' > libdarknet.path
+libdarknet=`cat libdarknet.path` 
+rm libdarknet.path
+
+sed -i "s/libdarknet.so/$libdarknet/" darknet/python/darknet.py 
+
+sed -i 's/data\/coco.names/darknet\/data\/coco.names/' darknet/cfg/coco.data 
+
+
+
